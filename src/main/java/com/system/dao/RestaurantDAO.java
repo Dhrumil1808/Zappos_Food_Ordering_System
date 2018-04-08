210688b179801b480838d49fd39135b3bc3ed744
Dhrumil1808 <dhrml.shah@gmail.com>
2018-04-07 23:25:00 -0700

    restaurant DAO

--- src/main/java/com/system/dao/RestaurantDAO.java
+++ src/main/java/com/system/dao/RestaurantDAO.java
@@ -1,19 +1,23 @@
 package com.system.dao;
 
 import java.util.List;
+
 import com.system.entity.*;
+
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface RestaurantDAO extends CrudRepository<Restaurant,Integer>{
+public interface RestaurantDAO extends JpaRepository<Restaurant,Integer>{
 	/**
 	 * @param number
 	 * @return
 	 */
-	List<Restaurant> findByName(String Name);
+	/*List<Restaurant> findByName(String Name);
 
 	List<Restaurant> getAllRestaurants();
     Restaurant getRestaurantById(int restaurantId);
@@ -23,5 +27,5 @@ public interface RestaurantDAO extends CrudRepository<Restaurant,Integer>{
 	/**
 	 * @param spec
 	 */
-	List<Restaurant> findAll(Specification spec);
+	//List<Restaurant> findAll(Specification<?> spec);
 }
