f29ceb31c71eddf028285ffbef01e8b244902657
Dhrumil1808 <dhrml.shah@gmail.com>
2018-04-08 17:55:03 -0700

    DAO updated

--- src/main/java/com/system/dao/RestaurantDAO.java
+++ src/main/java/com/system/dao/RestaurantDAO.java
@@ -1,13 +1,9 @@
 package com.system.dao;
 
 import java.util.List;
-
 import com.system.entity.*;
-
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
