47234222b88f3f9fa1ee8141c935c30919b13fb1
Dhrumil1808 <dhrml.shah@gmail.com>
2019-07-13 13:17:20 -0700

    code

--- src/main/java/com/system/controllers/RestaurantController.java
+++ src/main/java/com/system/controllers/RestaurantController.java
@@ -1,5 +1,6 @@
 package com.system.controllers;
 import java.util.ArrayList;
+
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
--- src/main/java/com/system/entity/Restaurant.java
+++ src/main/java/com/system/entity/Restaurant.java
@@ -2,6 +2,7 @@ package com.system.entity;
 
 
 import java.io.Serializable;
+
 import java.util.List;
 import java.util.Set;
 
